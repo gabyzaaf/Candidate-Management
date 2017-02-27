e2fe7f6d83048e348a5e928ccbd826e59a84ea10
GAMELIN Fabien <gamelin.fabien@europe.essilor.group>
2017-02-27 17:12:02 +0100

    add second solutions test

---
+++ test_connect_core/src/test_connect_core/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test_connect_core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Récupérer les bons settings json
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            var configuration = builder.Build();
+            string connectionString = configuration.GetConnectionString("SampleConnection");
+
+         
+            //Créer un utilisateur
+            var entry = new Utilisateur() { Name = "Julie", Mail = "julieG@gmail.com" };
+
+            using (var context = UtilisateurContextFactory.Create(connectionString))
+            {
+                context.Add(entry);
+                context.SaveChanges();
+            }
+
+            Console.WriteLine("Utilisateur créer en DB avec l'id : {entry.Id}");
+            Console.Read();
+
+        }
+    }
+}
---
+++ test_connect_core/src/test_connect_core/UtilisateurContextFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySQL.Data.EntityFrameworkCore.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test_connect_core
+{
+    public class UtilisateurContextFactory
+    {
+        public static UtilisateurContext Create(string connectionString)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<UtilisateurContext>();
+            optionsBuilder.UseMySQL(connectionString);
+
+            var context = new UtilisateurContext(optionsBuilder.Options);
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+
+        public static UtilisateurContext Authentifcation(string connectionString, string sql)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<UtilisateurContext>();
+            optionsBuilder.UseMySQL(connectionString);
+
+            var context = new UtilisateurContext(optionsBuilder.Options);
+            context.Database.ExecuteSqlCommand(sql);
+
+            return context;
+        }
+    }
+}
