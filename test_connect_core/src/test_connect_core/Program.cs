dd05b5a9b28ccab3bf5e673cee7442531f0a15c8
GAMELIN Fabien <gamelin.fabien@europe.essilor.group>
2017-02-28 15:38:13 +0100

    architecture améliorée

--- test_connect_core/src/test_connect_core/Program.cs
+++ test_connect_core/src/test_connect_core/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
+
 namespace test_connect_core
 {
     public class Program
@@ -15,19 +17,41 @@ namespace test_connect_core
             var configuration = builder.Build();
             string connectionString = configuration.GetConnectionString("SampleConnection");
 
-         
             //Créer un utilisateur
-            var entry = new Utilisateur() { Name = "Julie", Mail = "julieG@gmail.com" };
-
+            var entry = new Utilisateur() { Name = "rigolus", Mail = "rigolus@gmail.com", Mdp = "pwd" };
             using (var context = UtilisateurContextFactory.Create(connectionString))
             {
-                context.Add(entry);
+                context.Add(entry);   //Nous pouvons supprimer également context.Remove(entry)
                 context.SaveChanges();
+                Console.WriteLine("Utilisateur crée en DB avec l'id : " + entry.Id);
+
+             //Requete modifiable au choix
+                var query = from b in context.Utilisateurs
+                            orderby b.Name
+                            select b;
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
             }
 
-            Console.WriteLine("Utilisateur créer en DB avec l'id : {entry.Id}");
-            Console.Read();
+            var cand = new Candidat() { Name = "Timothée", LastName = "Dupont", Mail = "timodupont@gmail.com", toBeCalled = false };
+            using (var context = CandidatContextFactory.Create(connectionString))
+            {
+                context.Add(cand);      //Nous pouvons supprimer également context.Remove(cand)
+                context.SaveChanges();
 
+                //Requete modifiable au choix
+                var query = from b in context.candidats
+                            orderby b.Name
+                            select b;
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+
+                Console.ReadKey(); 
         }
     }
 }
