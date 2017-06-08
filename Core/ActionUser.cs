bdb5d3954ad256d57f4b1889573ba0f1062e1a80
gabyzaaf <zaafrani.prog@gmail.com>
2017-06-08 09:28:20 +0200

    change by mail kit for dotnet version 1

--- Core/ActionUser.cs
+++ Core/ActionUser.cs
@@ -2,7 +2,9 @@ using System;
 using System.IO;
 using actions;
 using exceptions;
-using System.Net.Mail;
+using MailKit.Net.Smtp;
+using MimeKit;
+using MailKit.Security;
 
 namespace actionUser{
 
@@ -27,43 +29,59 @@ namespace actionUser{
             string content = File.ReadAllText(user.fileName);
             return content.Replace("<prenom>","Gabriel").Replace("<pnom>","toto");
         }
-
-
-       public void sendEmail(string email,  string emailBody, string firstName)
+       
+         // string email,  string emailBody, string firstName
+         public void sendEmail(string email, string message,string firstName)
         {
+            try 
+            { 
+                //From Address 
+                string FromAddress = "upload.file.travail@gmail.com"; 
+                string FromAdressTitle = "Email from ASP.NET Core 1.1"; 
+                //To Address 
+                string ToAddress = email; 
+                string ToAdressTitle = "Microsoft ASP.NET Core"; 
+                string Subject = "Hello World - Sending email using ASP.NET Core 1.1"; 
+                string BodyContent = message; 
+    
+                //Smtp Server 
+                string SmtpServer = "smtp.gmail.com"; 
+                //Smtp Port Number 
+                int SmtpPortNumber = 587; 
+    
+                var mimeMessage = new MimeMessage(); 
+                mimeMessage.From.Add(new MailboxAddress(FromAdressTitle, FromAddress)); 
+                mimeMessage.To.Add(new MailboxAddress(ToAdressTitle, ToAddress)); 
+                mimeMessage.Subject = Subject; 
+                mimeMessage.Body = new TextPart("plain") 
+                { 
+                    Text = BodyContent 
+    
+                }; 
+    
+                using (var client = new SmtpClient()) 
+                { 
+    
+                    client.Connect(SmtpServer, SmtpPortNumber, false); 
+                    // Note: only needed if the SMTP server requires authentication 
+                    // Error 5.5.1 Authentication  
+                    client.Authenticate("upload.file.travail@gmail.com", "ESGI@2017"); 
+                    client.Send(mimeMessage); 
+                    Console.WriteLine("The mail has been sent successfully !!"); 
+                    client.Disconnect(true); 
+    
+                } 
+            } 
+            catch (Exception ex) 
+            { 
+                throw ex; 
+            } 
             
-        try{
-            if(String.IsNullOrEmpty(email)){
-                throw new Exception("the email is empty");
-            }
-            if(String.IsNullOrEmpty(emailBody)){
-                throw new Exception("the email body is empty");
-            }
-            MailAddress fromAddress = new MailAddress("upload.file.travail@gmail.com");
-            MailAddress toAddress = new MailAddress(email, firstName);
-            const string fromPassword = "ESGI@2017";
-            SmtpClient smtp = new SmtpClient();
-            smtp.Host = "smtp.gmail.com";
-            smtp.Port = 587;
-            smtp.EnableSsl = true;
-            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-            smtp.UseDefaultCredentials = false;
-            smtp.Credentials = new System.Net.NetworkCredential(fromAddress.Address, fromPassword);
-            MailMessage message = new MailMessage(fromAddress, toAddress);
-            message.Subject = "hello world";
-            message.Body = emailBody;
-            smtp.Send(message);
-        }catch(FormatException format){
-            Console.WriteLine($"the format error is {format.Message} for {email}");
         }
-        catch(EmailCustomException exc){
-            Console.WriteLine($"the error is {exc.Message}");
-        }
-        
+
+
     } 
 
+
        
-
-    }
-
 }
